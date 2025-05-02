2fe4a2bbff523e079a3451a7d664580578924194
ymatthew2466 <ymatthew2466@gmail.com>
2025-05-02 18:13:11 -0400

    added feat - measure signal strength, tilt and pan by signal

--- radio_receive/radio_receive.ino
+++ radio_receive/radio_receive.ino
@@ -1,12 +1,34 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <Stepper.h>
+#include <Servo.h>
 
-// Create RF24 object using CE pin 9 and CSN pin 8
+
+// CE pin 9 and CSN pin 8
 RF24 radio(9, 8);
 
-// Address for communication (must match on both devices)
+// Address for communication 
 const byte address[6] = "00001";
+
+// Stepper
+#define STEPS_PER_REV 100
+Stepper pan(STEPS_PER_REV, 8, 9, 10, 11);  // pins
+
+// Servo
+Servo tilt;
+
+// return # of N samples had RPD == 1 (greater than -64 dBm)
+int measureStrength(int N=20) {
+  int c = 0;
+  for (int i = 0; i < N; i++) {
+    if (radio.testRPD()){
+      c += 1;
+    }
+    delay(1);
+  }
+  return c;
+}
 
 void setup() {
   Serial.begin(9600);
@@ -29,7 +51,38 @@ void loop() {
   if (radio.available()) {
     char text[32] = {0};                  // Buffer for incoming message
     radio.read(&text, sizeof(text));      // Read message into buffer
-    Serial.print("Received: ");
-    Serial.println(text);
+    // Serial.print("Received: ");
+    // Serial.println(text);
+
+    // RPD bit
+    // 1 if greater than -64 dBm, 0 if less
+    Serial.println("" + radio.testRPD());
+
+
+    // XZ-axis (stepper motor)  
+    int basePan = measureStrength();
+    pan.step(1);
+    int upPan = measureStrength();
+
+    // if it's worse, then undo
+    if (upPan < basePan){
+      pan.step(-1);  
+    } 
+
+    // Y-axis (servo)
+    static int angle = 90;
+    int baseTilt = measureStrength();
+    tilt.write(angle + 1);
+    delay(150);
+    int upTilt = measureStrength();
+    if (upTilt >= baseTilt) {
+      angle = min(angle + 1, 180);
+    }
+    else {
+      tilt.write(angle - 1);
+      delay(150);
+    }
+
+    delay(100);
   }
 }
